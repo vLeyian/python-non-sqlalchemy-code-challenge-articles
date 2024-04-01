285547f1de32f48fbed1488a067e17afdb7edd38
Leyian <victor.leyian@student.moringaschool.com>
2024-04-01 09:11:37 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -6,8 +6,19 @@ from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
 
+    author_1 = Author("Carry Bradshaw")
+    author_2 = Author("Nathaniel Hawthorne")
+    author_3 = Author("Rene Acosta")
 
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+    magazine_1 = Magazine("Vogue", "Fashion")
+    magazine_2 = Magazine("AD", "Architecture")
+
+    article_3 = Article(author_1, magazine_2, "TITLE")
+    article_2 = Article(author_2, magazine_2, "Dating life in NYC")
+    article_5 = Article(author_3, magazine_2, "My Article")
+    article_3 = Article(author_3, magazine_2, "Other title")
+
+    article_1 = author_1.add_article(magazine_1, "How to wear a tutu with style")
+    
+    print(magazine_1.contributing_authors())
